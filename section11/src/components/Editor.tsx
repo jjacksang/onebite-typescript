9b98fcf12e94987b4d0e6813f12908993eb05cd0
Sangwoo <jswoo1995@gmail.com>
2024-12-15 12:46:46 +0900

    useContext

--- section11/src/components/Editor.tsx
+++ section11/src/components/Editor.tsx
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { TodoDispatchContext, useTodoDispatch } from "../App";
 
-interface Props {
-    onClickAdd: (text: string) => void;
-}
+interface Props {}
 
 export default function Editor(props: Props) {
     const [text, setText] = useState("");
+
+    const dispatch = useTodoDispatch();
 
     const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setText(e.target.value);
     };
 
     const onClickButton = () => {
-        props.onClickAdd(text);
+        dispatch.onClickAdd(text);
         setText("");
     };
 
